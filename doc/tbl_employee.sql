6c5b361a27822060f1315038eda14b3d52d0b228
DionQiu <m18759158365@163.com>
2018-08-12 00:07:48 +0800

    项目初始化

--- doc/tbl_employee.sql
+++ doc/tbl_employee.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : mysql
-Source Server Version : 50628
-Source Host           : gz-cdb-gm9ozszn.sql.tencentcdb.com:62183
-Source Database       : jxhstore
-
-Target Server Type    : MYSQL
-Target Server Version : 50628
-File Encoding         : 65001
-
-Date: 2018-08-12 00:05:43
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
