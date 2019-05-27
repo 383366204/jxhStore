2c16700500422cf01beb5f64839db9d4cf3bfce3
DionQiu <m18759158365@163.com>
2019-05-27 16:19:56 +0800

    优化

--- src/main/java/com/newland/jxh/store/common/jsonResult/ServerResponse.java
+++ src/main/java/com/newland/jxh/store/common/jsonResult/ServerResponse.java
@@ -1,4 +1,4 @@
-package com.newland.jxh.store.common.jsonResult;
+package com.yjcloud.asr.video.utils.jsonResult;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -6,18 +6,18 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.io.Serializable;
 
 /**
-* @Author qyw
-* @Description 服务响应通用对象
-* @Date Created in 20:51 2018/8/11
-* @Param
-* @Return
-**/
+ * @Author qyw
+ * @Description 服务响应通用对象
+ * @Date Created in 20:51 2019/5/27
+ **/
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class  ServerResponse<T> implements Serializable {
 
+    private static final long serialVersionUID = -206696267977993096L;
+    
     private  int code;
     private  String msg;
-    private T data;
+    private  T data;
 
     private ServerResponse(int code) {
         this.code = code;
@@ -69,44 +69,44 @@ public class  ServerResponse<T> implements Serializable {
     }
     @JsonIgnore
     public  boolean isSuccess(){
-        return  this.code==1;
+        return  this.code == 1;
     }
 
-    public  static <T> ServerResponse<T> createBySuccess(){
-        return  new ServerResponse<T>(ServerCode.SUCCESS.getCode());
+    public  static <T> ServerResponse<T> success(){
+        return  new ServerResponse<>(ServerCodeEnum.SUCCESS.getCode());
     }
 
-    public  static <T> ServerResponse<T> createBySuccess(String msg){
-        return  new ServerResponse<T>(ServerCode.SUCCESS.getCode(),msg);
+    public  static <T> ServerResponse<T> success(String msg){
+        return  new ServerResponse<>(ServerCodeEnum.SUCCESS.getCode(),msg);
     }
 
-    public  static <T> ServerResponse<T> createBySuccess(T data){
-        return  new ServerResponse<T>(ServerCode.SUCCESS.getCode(),data);
-    }
-
-    public  static <T> ServerResponse<T> createByError(String msg,T data){
-        return  new ServerResponse<T>(ServerCode.ERROR.getCode(),msg,data);
+    public  static <T> ServerResponse<T> success(T data){
+        return  new ServerResponse<>(ServerCodeEnum.SUCCESS.getCode(),data);
     }
 
 
-    public  static <T> ServerResponse<T> createBySuccess(String msg,T data){
-        return  new ServerResponse<T>(ServerCode.SUCCESS.getCode(),msg,data);
+    public  static <T> ServerResponse<T> success(String msg,T data){
+        return  new ServerResponse<>(ServerCodeEnum.SUCCESS.getCode(),msg,data);
     }
 
 
-    public  static <T> ServerResponse<T> createByError(){
-        return  new ServerResponse<T>(ServerCode.ERROR.getCode(),ServerCode.ERROR.getDesc());
+    public  static <T> ServerResponse<T> error(String msg,T data){
+        return  new ServerResponse<>(ServerCodeEnum.ERROR.getCode(),msg,data);
     }
 
-    public  static <T> ServerResponse<T> createByError(String errorMsg){
-        return  new ServerResponse<T>(ServerCode.ERROR.getCode(),errorMsg);
+    public  static <T> ServerResponse<T> error(){
+        return  new ServerResponse<>(ServerCodeEnum.ERROR.getCode(),ServerCodeEnum.ERROR.getDesc());
     }
 
-    public  static <T> ServerResponse<T> createByError(int code,String errorMsg){
-        return  new ServerResponse<T>(code,errorMsg);
+    public  static <T> ServerResponse<T> error(String errorMsg){
+        return  new ServerResponse<>(ServerCodeEnum.ERROR.getCode(),errorMsg);
     }
 
-    public  static <T> ServerResponse<T> createByError(int code,String errorMsg,T date){
-        return  new ServerResponse<T>(code,errorMsg,date);
+    public  static <T> ServerResponse<T> error(int code,String errorMsg){
+        return  new ServerResponse<>(code,errorMsg);
     }
- }
+
+    public  static <T> ServerResponse<T> error(int code,String errorMsg,T date){
+        return  new ServerResponse<>(code,errorMsg,date);
+    }
+}
